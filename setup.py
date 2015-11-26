838aa6639419084d393a27abae00326acb71a956
Alfredo Mungo <alfredo.mungo@openmailbox.org>
2015-11-27 00:23:35 +0100

    Added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+  name='netstring',
+  description='Netstrings module for Python 3',
+  author='Alfredo Mungo',
+  author_email='alfredo.mungo@openmailbox.org',
+  url='https://github.com/alkafir/netstring',
+  version='0.1',
+  py_modules=('netstring',)
+)
